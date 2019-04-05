cc84b539b6e7608bbfda3c2b4bbe53d9b78d41bc
mertww0416 <47248534+mertww0416@users.noreply.github.com>
2019-04-05 14:21:33 +0200

    Add files via upload

--- Target.js
+++ Target.js
@@ -1,21 +1,18 @@
+//dit spawnt de balletjes.
 class Target {
 	constructor(x, y, r) {
 		this.x = x;
 		this.y = y;
 		this.r = r;
 	}
-	
+	//dit zorgt ervoor dat als je op de balletjes klikt ze ook daadwerkelijk een functie hebben.
 	contains(x, y) {
 		let d = dist(x, y, this.x, this.y);
-		if (d < this.r) {
-			return true;
-		} else {
-			return false;	
-		}
+		return d < this.r;
 	}
-	
+	//laat de balletjes zien.
 	show() {
-		fill(255)
+		fill(255);
         ellipse(this.x, this.y, this.r*2)
 	}
 }
--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+//maak variabelen aan
 let targets = [];
 let interval;
 let counter = 0;
@@ -6,24 +7,26 @@ let button;
 let windowWidth;
 let windowHeight;
 let started = false;
-
+//
 function setup() {
+	//dit laat de scherm zien van het spelletje
 	windowWidth = window.innerWidth;
 	windowHeight = window.innerHeight;
-  	createCanvas(windowWidth, windowHeight);	
+  	createCanvas(windowWidth, windowHeight);
 
+  	//startknop, puntenteller en puntenverberger
 	button = createButton('start');
 	score = createP(counter);
 	score.hide();
 }
-
+//hier word bepaalt waar en dat de balletjes komen.
 function timeIt() {
 	let x = random(width);
 	let y = random(height);
 	let target = new Target(x, y, 35);
 	targets.push(target);
 }
-
+//als er word geklikt met de muis wordt deze functie uitgevoerd.
 function mouseClicked() {
 	if (started === false) {
 		startGame();
@@ -43,18 +46,21 @@ function mouseClicked() {
     }
 }
 
+//dit is een functie die word uitgevoerd als je de game hebt gestart.
 function startGame() {
     counter = 0;
     started = true;
-	interval = setInterval(timeIt, 500);
-	button.hide();
-	score.show();
+    interval = setInterval(timeIt, 500);
+    button.hide();
+    score.show();
 }
 
+//dit is een functie die de website opnieuw laadt als je hebt verloren.
 function stopGame() {
 	location.reload();
 }
 
+//dit is een functie die de aantal ballen telt. Als het meer als 6 is heb je verloren en wordt de pagina gereload. Ook kan je hier de achtergrond kleur wijzigen.
 function draw() {
 	background(0, 0, 255);
 	if (counter >= 0 && targets.length >= 0 && targets.length <= 6) {
